fix masking

src/model.py
@@ -211,7 +211,7 @@ class Model(nn.Module):
         distortion_loss = self.distortion_loss(x_gen, x_real)
         perceptual_loss = self.perceptual_loss_wrapper(x_gen, x_real, normalize=True)
 
-        mask_distortion = self.distortion_loss(x_gen * mask, x_real * mask)
+        mask_distortion = self.distortion_loss(x_gen[mask], x_real[mask])
 
         # TODO: args.k_M?
         weighted_distortion = self.args.k_M * distortion_loss + 10 * self.args.k_M * mask_distortion