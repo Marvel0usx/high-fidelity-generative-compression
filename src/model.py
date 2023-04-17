fix masking; unsqueeze_and_transpose

src/model.py
@@ -211,7 +211,8 @@ class Model(nn.Module):
         distortion_loss = self.distortion_loss(x_gen, x_real)
         perceptual_loss = self.perceptual_loss_wrapper(x_gen, x_real, normalize=True)
 
-        mask_distortion = self.distortion_loss(x_gen[mask.unsqueeze(1)], x_real[mask.unsqueeze(1)])
+        mask_distortion = self.distortion_loss(torch.transpose(x_gen,0,1)[mask.unsqueeze(0)],
+                                               torch.transpose(x_real,0,1)[mask.unsqueeze(0)])
 
         # TODO: args.k_M?
         weighted_distortion = self.args.k_M * distortion_loss + 10 * self.args.k_M * mask_distortion