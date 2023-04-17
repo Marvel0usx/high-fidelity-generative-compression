63a0a38d8329c869467f6c1f397028dc031c00f7
Wang Yuxi <wang1179@teach.cs.toronto.edu>
2023-04-16 23:26:57 -0400

    print test

--- train.py
+++ train.py
@@ -65,6 +65,7 @@ def test(args, model, epoch, idx, data, test_data, test_mask, test_bpp, device, 
     with torch.no_grad():
         data = data.to(device, dtype=torch.float)
         test_mask = test_mask.to(device, dtype=torch.float)
+        print(("C" * 40) + " test: ", test_mask.shape)
 
         losses, intermediates = model(data, test_mask, return_intermediates=True, writeout=False)
         utils.save_images(train_writer, model.step_counter, intermediates.input_image, intermediates.reconstruction,
@@ -115,6 +116,7 @@ def train(args, model, train_loader, test_loader, device, logger, optimizers):
         for idx, (data, bpp, mask) in enumerate(tqdm(train_loader, desc='Train', position=0, leave=True), 0):
             data = data.to(device, dtype=torch.float)
             mask = data.to(device, dtype=torch.float)
+            print("B" * 39, f"{mask.shape}")
             
             try:
                 if model.use_discriminator is True:
